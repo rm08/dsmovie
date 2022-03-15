2e0f0ade6b1fe3fc84c8b7f39a59189da0bd6592
rm08 <rafael.massinatore@gmail.com>
2022-03-15 20:02:26 -0300

    Movie Card

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,9 +1,13 @@
 import Pagination from "components/Pagination";
+import MovieCard from "components/MovieCard";
 import './styles.css'
 
 function Listing() {
     return (
+        <>
         <Pagination />
+        <MovieCard />
+        </>
     );
 }
 
